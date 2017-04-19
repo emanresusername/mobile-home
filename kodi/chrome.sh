7dbe38d2de032641d1a0741363ab38e8a0a2adff
mandroid <emailme.jtg+github@gmail.com>
2017-04-19 01:14:24 -0700

    clean up kodi -> chromecast shortcuts

--- kodi/chrome.sh
+++ kodi/chrome.sh
@@ -1,3 +1,5 @@
+set -e
+
 destination="/sdcard/Android/data/org.xbmc.kodi/files/.kodi/userdata/playercorefactory.xml"
 command=$1
 kodiuser=u0_a306
@@ -5,27 +7,9 @@ kodigroup=sdcard_rw
 
 if [ "$command" == "update" ]
 then
-    cat << EOF > "$destination"
-<playercorefactory>
-    <players>
-        <player name="LocalCast" type="ExternalPlayer" audio="false" video="true">
-            <filename>de.stefanpledl.localcast</filename>
-            <hidexbmc>true</hidexbmc>
-            <playcountminimumtime>120</playcountminimumtime>
-        </player>
-    </players>
-    <rules action="prepend">
-        <rule protocols="smb" player="LocalCast" />
-        <rule dvdimage="true" player="LocalCast" />
-        <rule protocols="rtmp" player="LocalCast" />
-        <rule protocols="rtsp" player="LocalCast" />
-        <rule protocols="sop" player="LocalCast" />
-        <rule internetstream="true" player="LocalCast" />
-        <rule video="true" player="LocalCast" /> <!-- Default for anything else not listed -->
-    </rules>
-</playercorefactory>
-EOF
-    chown -R $kodiuser:$kodigroup "$destination"
+    source="$HOME/kodi/playercorefactory.xml"
+    cp "$source" "$destination"
+    chown $kodiuser:$kodigroup "$destination"
     echo 'kodi -> chrome'
 elif [ "$command" == "remove" ]
 then
